2112f28cb13c43e45c5b50b61ce1100848024581
mertttekin <merttekin.bm@gmail.com>
2023-05-10 17:55:46 +0300

    add some fatures

---
+++ pages/modify.vue
@@ -0,0 +1,23 @@
+<template>
+    <a href="#" @click="$event=>view('welcome')">Show Welcome</a>
+    <a href="#" @click="$event=>view('counter')">Show Counter</a>
+
+    <Counter v-if="page =='counter'"/>
+    <Welcome v-if="page =='welcome'"/>
+</template>
+
+<script setup>
+
+const page = ref("welcome")
+
+function view(name){
+    page.value = name;
+}
+
+</script>
+
+<style>
+    a {
+        margin-right: 10px;
+    }
+</style>
