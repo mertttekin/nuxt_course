bde7c26961957eabbf9ec54c6015ad2b17ed68e9
mertttekin <merttekin.bm@gmail.com>
2023-05-10 14:17:33 +0300

    first-commit

---
+++ components/AppAlert.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Welcome to the App Alerttttt</h1>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <nav>
+      <ul class="navbar">
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+        <li><NuxtLink to="/blog">Blog</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style>
+    .navbar{
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      list-style-type: none;}
+</style>
